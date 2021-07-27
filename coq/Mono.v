642a110095a65dbf26d084aae0eb66485953d97a
rnp39 <rnp39@cornell.edu>
2021-07-26 19:57:41 -0700

    some monotype constraint typing

--- coq/Mono.v
+++ coq/Mono.v
@@ -1,23 +1,77 @@
-Require Import Coq.Strings.String.
-Require Import CoqRecon.Util.
+Require Import Coq.Strings.String CoqRecon.Util.
 
-Inductive type : Set :=
+Instance StringEqDec : EqDec string eq := {equiv_dec := string_dec}.
+
+Inductive typ : Set :=
 | TBool
 | TNat
-| TArrow : type -> type -> type
-| TVar : nat -> type.
+| TArrow : typ -> typ -> typ
+| TVar : nat -> typ.
+
+Declare Scope typ_scope.
+Delimit Scope typ_scope with typ.
+
+Notation "t1 → t2"
+  := (TArrow t1 t2)
+       (at level 30, right associativity) : typ_scope.
+
+Fixpoint tvars (t : typ) : list nat :=
+  match t with
+  | TBool | TNat => []
+  | TVar T => [T]
+  | (t → t')%typ => tvars t ++ tvars t'
+  end.
+
+Definition Ctvars : list (typ * typ) -> list nat :=
+  fold_right (fun '(l,r) acc => tvars l ++ tvars r ++ acc) [].
 
 Inductive op : Set :=
 | And | Or | Add | Sub | Eq | Lt.
 
 Inductive term : Set :=
 | Var : string -> term
-| Abs : string -> term
+| Abs : string -> term -> term
 | App : term -> term -> term
 | Bool : bool -> term
+| Nat : nat -> term
 | Cond : term -> term -> term
 | Op : op -> term -> term.
 
-Definition gamma : Set := @env string type.
+Declare Scope term_scope.
+Delimit Scope term_scope with term.
 
-Inductive typing (g : gamma) : term -> type -> list string -> Prop -> Prop :=.
+Notation "'λ' x ⇒ e"
+  := (Abs x e)
+       (at level 35, right associativity) : term_scope.
+Notation "e1 ⋅ e2"
+  := (App e1 e2)
+       (at level 34, left associativity) : term_scope.
+
+Definition gamma : Set := @env string typ.
+
+Reserved Notation "g ⊢ e ∈ t ⊣ X @ C" (at level 70).
+
+Inductive constraint_typing (Γ : gamma)
+  : term -> typ -> list nat -> list (typ * typ) -> Prop :=
+| ct_bool b :
+    Γ ⊢ Bool b ∈ TBool ⊣ [] @ []
+| ct_nat n :
+    Γ ⊢ Nat n ∈ TNat ⊣ [] @ []
+| ct_var x τ :
+    bound x τ Γ ->
+    Γ ⊢ Var x ∈ τ ⊣ [] @ []
+| ct_abs x e T τ X C :
+    ~ In T X ->
+    (x ↦ TVar T ;; Γ)%env ⊢ e ∈ τ ⊣ X @ C ->
+    Γ ⊢ (λ x ⇒ e)%term ∈ (TVar T → τ)%typ ⊣ (T :: X) @ C
+| ct_app e1 e2 T τ1 τ2 X1 X2 C1 C2 :
+    (X1 ∩ X2)%set = [] ->
+    (X1 ∩ tvars τ2)%set = [] ->
+    (X2 ∩ tvars τ1)%set = [] ->
+    ~ In T (X1 ++ X2 ++ tvars τ1 ++ tvars τ2 ++ Ctvars C1 ++ Ctvars C2) ->
+    Γ ⊢ e1 ∈ τ1 ⊣ X1 @ C1 ->
+    Γ ⊢ e2 ∈ τ2 ⊣ X2 @ C2 ->
+    Γ ⊢ (e1 ⋅ e2)%term ∈ TVar T
+      ⊣ (T :: X1 ++ X2) @ ((τ1, (τ2 → TVar T)%typ) :: C1 ++ C2)
+where "g ⊢ e ∈ t ⊣ X @ C"
+        := (constraint_typing g e t X C).
--- coq/Util.v
+++ coq/Util.v
@@ -1,9 +1,18 @@
-Require Export Coq.Classes.EquivDec.
+Require Export Coq.Classes.EquivDec Coq.Lists.List Coq.Bool.Bool.
+Export ListNotations.
+
+Ltac inv H := inversion H; subst; clear H.
+
+Definition reflects
+           {A B : Type} (R : A -> B -> Prop) (f: A -> B -> bool) :=
+  forall a b, reflect (R a b) (f a b).
 
 Section Env.
   Context {K V : Set}.
 
   Definition env : Set := K -> option V.
+
+  Definition empty : env := fun _ => None.
   
   Context {HDK : EqDec K eq}.
 
@@ -11,4 +20,140 @@ Section Env.
     fun k' => if equiv_dec k' k then Some v else e k'.
 
   Definition find (k : K) (e : env) : option V := e k.
+
+  Definition bound (k : K) (v : V) (e : env) : Prop := e k = Some v.
 End Env.
+
+Declare Scope env_scope.
+Delimit Scope env_scope with env.
+
+Notation "'∅'" := empty (at level 0, no associativity) : env_scope.
+Notation "k ↦ v ';;' e"
+  := (bind k v e)
+       (at level 11, right associativity) : env_scope.
+(*Notation "k ↦ v ∈ e"
+  := (bound k v e) (at level 10, no associativity) : env_scope.*)
+
+Declare Scope set_scope.
+Delimit Scope set_scope with set.
+
+Reserved Notation "l ∪ r" (at level 50, left associativity).
+Reserved Notation "l ∩ r" (at level 49, left associativity).
+Reserved Notation "l ∖ r" (at level 48, left associativity).
+
+Section Sets.
+  Context {A : Set}.
+
+  (** [u] is the union of [l] & [r]. *)
+  Definition Union (l r u : list A) : Prop :=
+    forall a, In a u <-> In a l \/ In a r.
+
+  (** [i] is the intersection of [l] & [r]. *)
+  Definition Intersection (l r i : list A) : Prop :=
+    forall a, In a i <-> In a l /\ In a r.
+
+  (** [d] is the diff of [l] & [r]. *)
+  Definition Difference (l r d : list A) : Prop :=
+    forall a, In a d <-> In a l /\ ~ In a r.
+  
+  Lemma append_Union : forall l r, Union l r (l ++ r).
+  Proof.
+    unfold Union. auto using in_app_iff.
+  Qed.
+
+  Context {HEA: EqDec A eq}.
+
+  Fixpoint member (a : A) (l : list A) : bool :=
+    match l with
+    | [] => false
+    | h::t => if a == h then true else member a t
+    end.
+
+  Lemma In_member : forall a l, In a l -> member a l = true.
+  Proof.
+    intros a l; induction l as [| h t IHt];
+      intro H; simpl in *;
+        [ contradiction | destruct H as [H | H]]; subst;
+          match goal with
+          | |- context [if equiv_dec ?a ?b then _ else _]
+            => destruct (equiv_dec a b);
+                unfold equiv in *; subst; auto
+          end.
+  Qed.
+
+  Local Hint Resolve In_member : core.
+  
+  Lemma member_In : forall a l, member a l = true -> In a l.
+  Proof.
+    intros a l; induction l as [| h t IHt];
+      intro H; simpl in *; try discriminate.
+    destruct (equiv_dec a h) as [Hah | Hah];
+      unfold equiv in *; subst; auto.
+  Qed.
+
+  Local Hint Resolve member_In : core.
+  
+  Lemma In_member_reflects : reflects (@In A) member.
+  Proof.
+    Local Hint Constructors reflect : core.
+    unfold reflects.
+    intros a l; destruct (member a l) eqn:Hmem; auto.
+    constructor. intros H. apply In_member in H.
+    rewrite H in Hmem. discriminate.
+  Qed.
+  
+  Fixpoint intersect (l r : list A) : list A :=
+    match l with
+    | [] => []
+    | h::t =>
+      (if member h r then [h] else []) ++ intersect t r
+    end.
+
+  Lemma Intersection_intersect : forall l r,
+      Intersection l r (intersect l r).
+  Proof.
+    unfold Intersection; intro l;
+      induction l as [| h t IHt];
+      intros r a; simpl in *.
+    - intuition.
+    - split.
+      + pose proof (In_member_reflects h r) as H; inv H; simpl.
+        * intros [H | H]; subst; firstorder.
+        * firstorder.
+      + intros [[Hha | Hat] Har]; subst.
+        * rewrite In_member by auto; simpl; auto.
+        * rewrite in_app_iff. firstorder.
+  Qed.
+
+  Fixpoint difference (l r : list A) : list A :=
+    match l with
+    | [] => []
+    | h::t =>
+      (if member h r then [] else [h]) ++ difference t r
+    end.
+
+  Lemma Difference_difference : forall l r,
+      Difference l r (difference l r).
+  Proof.
+    unfold Difference; intro l;
+      induction l as [| h t IHt];
+      intros r a; simpl in *.
+    - intuition.
+    - split.
+      + pose proof In_member_reflects h r as H; inv H; simpl.
+        * intros H; apply IHt in H as [IH IHr]. intuition.
+        * intros [H | H]; subst; intuition;
+            apply IHt in H; intuition.
+      + intros [[Hha | Hat] Har]; subst.
+        * assert (member a r = false).
+          { pose proof In_member_reflects a r as H; inv H;
+              auto; contradiction. }
+          rewrite H. rewrite in_app_iff. intuition.
+        * rewrite in_app_iff. right.
+          apply IHt. intuition.
+  Qed.
+End Sets.
+
+Notation "l ∪ r" := (l ++ r) : set_scope.
+Notation "l ∩ r" := (intersect l r) : set_scope.
+Notation "l ∖ r" := (difference l r) : set_scope.
